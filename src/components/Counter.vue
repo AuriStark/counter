01189c4251bf29a7d621563b4278f5148d41bfa2
Aurelle Wafo <59320937+AuriStark@users.noreply.github.com>
2021-04-01 10:23:32 +0200

    a New Counter Component added

---
+++ src/components/Counter.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>This is a Counter.</h1>
+        <p @click="increment"> Click me to increment.</p>
+        <p>  You have Clicked {{ total }} Times. </p>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name:'Counter',
+    data(){
+        return{
+            total:0
+        }
+    },
+    methods:{
+        increment(){
+            return this.total += 1;
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
